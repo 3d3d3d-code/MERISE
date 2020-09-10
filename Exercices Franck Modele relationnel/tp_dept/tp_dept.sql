fda455ecc2296a17555bd23508ed05dde052382f
3d3d3d-code <reda.hamza8@orange.fr>
2020-09-10 13:56:27 +0200

    SUITE EXO SQL 10 09 20

--- Exercices Franck Modele relationnel/tp_dept/tp_dept.sql
+++ Exercices Franck Modele relationnel/tp_dept/tp_dept.sql
@@ -9,7 +9,10 @@ CREATE TABLE dept (
   DEPTNO int(10) UNSIGNED NOT NULL,
   DNAME varchar(50) NOT NULL,
   LOC varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) 
+ENGINE = InnoDB
+ CHARSET utf8
+ COLLATE utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -26,8 +29,10 @@ CREATE TABLE `emp` (
   SAL decimal(8,2) NOT NULL,
   COMM int(11) DEFAULT NULL,
   DEPTNO int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) 
+ENGINE = InnoDB
+ CHARSET utf8
+ COLLATE utf8_general_ci;
 -- Déchargement des données de la table `dept`
 --
 
@@ -64,17 +69,17 @@ COMMIT;
 -- QUESTION 1 : Donner nom, job, numéro et salaire de tous les employés, puis seulement des employés du département 10
 
 
-SELECT `ENAME`,`JOB`,`EMPNO`FROM `emp` ; 
+SELECT `ENAME`,`JOB`,`EMPNO`,`SAL` FROM `emp` ; 
 
 SELECT `ENAME`,`JOB`,`EMPNO`FROM `emp` WHERE `DEPTNO`=10 ; 
 
 -- QESTION 2 : Donner nom, job et salaire des employés de type MANAGER dont le salaire est supérieur à 2800
 
-SELECT `ENAME`,`JOB`,`EMPNO`FROM `emp` WHERE  `JOB` LIKE'MANAGER' AND `SAL`> 2800 ; 
+SELECT `ENAME`,`JOB`,`EMPNO`FROM `emp` WHERE  `JOB`='MANAGER' AND `SAL`> 2800 ; 
 
 -- QUESTION 3:
 
-SELECT `JOB` FROM `emp` WHERE `JOB`LIKE 'MANAGER' AND DEPTNO != 30;
+SELECT `JOB` FROM `emp` WHERE `JOB`= 'MANAGER' AND DEPTNO != 30;
 
 -- QUESTION 4 : Liste des employés de salaire compris entre 1200 et 1400
 
@@ -94,22 +99,39 @@ SELECT `ENAME` FROM `emp`    WHERE `DEPTNO`  ORDER BY (`JOB`)  ;
 
 -- QUESTION 8 Liste des différents emplois
 
-SELECT `JOB` FROM `emp`;
+SELECT DISTINCT `JOB` FROM `emp`;
 
--- QUESTION 9: Donner le nom du département où travaille ALLEN
+-- OU BIEN 
 
-SELECT `LOC`,`JOB` FROM `emp` INNER JOIN `dept` WHERE `ENAME` = 'ALLEN' ;                                                                   
+SELECT  `JOB` FROM `emp` GROUP BY `JOB`;
 
--- QUESTION 10 Liste des employés avec nom du département, nom, job, salaire classés par noms de départements et par salaires décroissants.
+-- QUESTION 9: Donner le nom du département où travaille ALLEN 
+
+SELECT dept.DNAME FROM emp INNER JOIN dept ON emp.DEPTNO  WHERE ENAME  = 'ALLEN' ; 
+
+SELECT dept.DNAME FROM  emp,dept WHERE emp.DEPTNO =dept.DEPTNO AND  ENAME ='ALLEN' ;
+                                                                
+
+-- QUESTION 10 : Liste des employés avec nom du département, nom, job, salaire classés par noms de départements et par salaires décroissants.
 
  SELECT `DNAME`,`ENAME`,`JOB`,`SAL` FROM `emp` INNER JOIN `dept`   ORDER BY  (`DNAME`) AND  (`SAL`)DESC;
  
- -- QUESTION 11  Liste des employés vendeurs (SALESMAN) avec affichage de nom, salaire, commissions, salaire + commissions
+ -- QUESTION 11 : Liste des employés vendeurs (SALESMAN) avec affichage de nom, salaire, commissions, salaire + commissions
 
 SELECT `ENAME`,`SAL`,`COMM`,(`SAL` + `COMM`) FROM `emp` WHERE `JOB`= 'SALESMAN' ;
 
+
+
 -- QUESTION 12  Liste des employés du département 20: nom, job, date d'embauche sous forme VEN 28 FEV 1997'
-SELECT `ENAME`||`JOB`||TO_CHAR(HIREDATE,'DAY, DD MONTH YYYY')FROM `emp`    WHERE `DEPTNO` IN(20)  ORDER BY (`ENAME`)    ; 
+SELECT `ENAME`||`JOB`||TO_CHAR(HIREDATE,'DAY, DD MONTH YYYY')FROM `emp` WHERE `DEPTNO` IN(20)  ORDER BY (`ENAME`)    ; 
+
+-- QUESTION 13 : Donner le salaire le plus élevé par département
+
+SELECT * FROM`emp` where `SAL`ORDER BY `DEPTNO`;
+
+-- QUESTION 14 : Donner département par département masse salariale, nombre d'employés, salaire moyen par type d'emploi.
+
+
 
 
 
