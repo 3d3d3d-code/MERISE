85532368cc20623a4bb8013e86af00ee8fe1ea53
3d3d3d-code <reda.hamza8@orange.fr>
2020-10-01 11:42:42 +0200

    correction exo 1 view_cities

--- tp_agence_voyages/_exercices_views.sql
+++ tp_agence_voyages/_exercices_views.sql
@@ -2,12 +2,17 @@ use tp_agence_voyages;
 
 
 -- Créer les vues répondant aux demandes suivantes :
-
-
+-- SELECT*FROM CITIES INNER JOIN COUNTRIES ;
 -- 1) Afficher les informations de toutes les villes (informations du pays incluses)
+DROP VIEW IF EXISTS view_cities ;
+CREATE VIEW view_cities
+AS
+SELECT cities.*,countries.country_name
+from countries
+JOIN cities ON cities.country_code = countries.country_code ;
 
 
-CREATE VIEW CITIES_SEE(citiy_code,city_name) AS SELECT *FROM CITIES INNER JOIN COUNTRIES
+
 
 
 
@@ -16,6 +21,7 @@ CREATE VIEW CITIES_SEE(citiy_code,city_name) AS SELECT *FROM CITIES INNER JOIN C
 
 -- 2) Afficher les noms, prénoms, email de tous les clients qui ont réservé au moins 1 voyage. 
 --    On a besoin de connaitre le titre du voyage ainsi que le pays de destination
+ 
 
 
 
